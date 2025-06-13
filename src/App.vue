a5c479003b470e289acd50d2f0dc9231bffaa40a
Ryan Pham <ryan_pham@fastboy.net>
2025-06-13 02:13:13 -0500

    Added Shopping list Demo

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,96 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+// @ts-nocheck
+import { ref, computed } from 'vue'
+
+const header = ref('Shopping List App')
+const editing = ref(false)
+const items = ref([
+  {
+    id: 1, 
+    label: "10 party hats", 
+    purchased: true,
+    highPriority: false
+  },
+  {
+    id: 2, 
+    label: "2 board games", 
+    purchased: true,
+    highPriority: false
+  },
+  {
+    id: 3, 
+    label: "20 cups", 
+    purchased: false,
+    highPriority: true
+  }
+])
+const reverssedItems = computed(()=>[...items.value].reverse())
+const newItem = ref("")
+const newItemHighPriority = ref(false)
+const saveItem = ()=>{
+  items.value.push(
+    {
+      id: items.value.length + 1, 
+      label: newItem.value,
+      highPriority: newItemHighPriority.value
+    })
+  newItem.value = ""
+  newItemHighPriority.value = ""
+}
+const doEdit = (e)=>{
+  editing.value = e
+  newItem.value = ""
+  newItemHighPriority.value = ""
+}
+const togglePurchased = (item) =>{
+  item.purchased = !item.purchased
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button v-if="editing" class="btn" @click="doEdit(false)">
+      Cancel
+    </button>
+    <button v-else class="btn btn-primary" @click="doEdit(true)">
+      Add Item
+    </button>
+  </div>
+  <form 
+  class="add-item-form"
+  v-if="editing"
+  @submit.prevent="saveItem" 
+>
+<input 
+  v-model.trim="newItem"
+  type="text" 
+  placeholder="Add an item">
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority">
+    High Priority
+  </label>
+  <button 
+    :disabled="newItem.length < 5"
+    class="btn btn-primary"
+  >
+    Save Item
+  </button>
+  </form>
+  <ul>
+    <li 
+      v-for="(item, index) in reverssedItems" 
+      @click="togglePurchased(item)"
+      :key="item.id"
+      class="static-class"
+      :class="{
+        strikeout: item.purchased, 
+        priority: item.highPriority}"
+    >
+      {{ item.label }}
+    </li>
+  </ul>
+  <p v-if="!items.length">
+    Nothing to see here
+  </p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
